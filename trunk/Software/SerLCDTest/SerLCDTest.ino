630d8ac4752fd13f687062404034585eccb385cb
tkoenig@gmail.com <tkoenig@gmail.com@a2582180-7de2-9f74-28df-986759cb4181>
2013-04-24 03:26:01 +0000

    Simple library for sparkfun and compatible serial LCD displays

---
+++ trunk/Software/SerLCDTest/SerLCDTest.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+#include <SFSerLCD.h>
+
+SFSerLCD LCD = SFSerLCD(2);
+
+void setup()
+{
+LCD.clearLCD();
+LCD.printStr("Aren't we going to dagobah?");
+LCD.goTo(25);
+LCD.printStr("NOW!");
+LCD.backlight50();
+}
+
+void loop()
+{
+delay(100);
+}
+
+
